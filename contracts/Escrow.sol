afae16bd22bad661fecef8012b05360d49aef166
Peter Novak <peter.novak.m@gmail.com>
2022-11-19 10:30:35 +0800

    Deposit usecase of Realestate logic

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,24 +1,54 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
 
+
+// an alternative to importing the file. "I" stands for Interface
 interface IERC721 {
     function transferFrom(address _from, address _to, uint256 _id) external;
 }
 
 contract Escrow {
     address public nftAddress;
-    uint public nftID;
+    uint256 public nftID;
+    uint256 public purchasePrice;
+    uint256 public escrowAmount;
+    address payable public seller;
+    address payable public buyer;
+    address public inspector;
+    address public lender;
 
-    constructor(address _nftAddress, uint256 _nftID) {
+    constructor(
+        address _nftAddress,
+        uint256 _nftID,
+        uint256 _purchasePrice,
+        uint256 _escrowAmount,
+        address payable _seller,
+        address payable _buyer,
+        address _inspector,
+        address _lender
+        ) {
         nftAddress = _nftAddress;
         nftID = _nftID;
+        purchasePrice = _purchasePrice;
+        escrowAmount = _escrowAmount;
+        seller = _seller;
+        buyer = _buyer;
+        inspector = _inspector;
+        lender = _lender;
+    }
 
+    function depositEarnest() public payable {
+        
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
     }
 
     // Transfer ownership of property
     function finalizeSale() public {
         // transfer ownership of property
-
+        IERC721(nftAddress).transferFrom(seller, buyer, nftID);
     }
 
 }
--- test/RealEstate.js
+++ test/RealEstate.js
@@ -1,34 +1,80 @@
 const { expect } = require('chai')
 const { ethers } = require('hardhat')
 
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+const ether = tokens
+
 describe('RealEstate', () => {
-    let realEstate, escrow
-    let deployer, seller
-    let nftID = 1
+  let realEstate, escrow
+  let deployer, seller
+  let nftID = 1
+  let purchasePrice = ether(100)
+  let escrowAmount = ether(100)
 
-    beforeEach(async () => {
-        // Setup accounts
-        accounts = await ethers.getSigners()
-        deployer = await accounts[0]
-        seller = deployer
+  beforeEach(async () => {
+    // Setup accounts
+    accounts = await ethers.getSigners()
+    deployer = await accounts[0]
+    seller = deployer
+    buyer = accounts[1]
+    inspector = accounts[2]
+    lender = accounts[3]
 
-        // Load contracts
-        const RealEstate = await ethers.getContractFactory('RealEstate')
-        const Escow = await ethers.getContractFactory('Escrow')
-        
-        // Deploy contracts
-        realEstate = await RealEstate.deploy()
-        escrow = await Escow.deploy(
-            realEstate.address,
-            nftID
+    // Load contracts
+    const RealEstate = await ethers.getContractFactory('RealEstate')
+    const Escow = await ethers.getContractFactory('Escrow')
+
+    // Deploy contracts
+    realEstate = await RealEstate.deploy()
+    escrow = await Escow.deploy(
+        realEstate.address,
+        nftID,
+        purchasePrice,
+        escrowAmount,
+        seller.address,
+        buyer.address,
+        inspector.address,
+        lender.address,
         )
-    }) 
 
-    describe('Deployment', async () => {
-        it('sends an NFT to the seller / deployer', async () => {
-            expect(await realEstate.ownerOf(nftID)).to.equal(seller.address)
-        })
+    // Seller Approves NFT
+    transaction = await realEstate.connect(seller).approve(escrow.address, nftID)
+    await transaction.wait()
+
+  })
+
+  describe('Deployment', async () => {
+    it('sends an NFT to the seller / deployer', async () => {
+      expect(await realEstate.ownerOf(nftID)).to.equal(seller.address)
+    })
+  })
+
+  describe('Selling real estate', async () => {
+    let balance, transaction
+
+    it('Executes a successful transaction', async () => {
+      // Expects seller to be NFT owner before the sale
+      expect(await realEstate.ownerOf(nftID)).to.equal(seller.address)
+
+      // Buyer deposit earnest
+      transaction = await escrow.connect(buyer).depositEarnest({value: escrowAmount})
+
+      // Check escrow balance
+      balance = await escrow.getBalance()
+      console.log("escrow balance:", ethers.utils.formatEther(balance))
+
+      // Finalize sale
+      transaction = await escrow.connect(buyer).finalizeSale()
+      await transaction.wait()
+      console.log('Buyer finalize sale')
+
+      // Expects buyer to be NFT owner after the sale
+      expect(await realEstate.ownerOf(nftID)).to.equal(buyer.address)
 
     })
 
+  })
 })
